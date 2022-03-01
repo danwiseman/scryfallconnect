4af07f5d2e841e6f121d5a43bfa14cf4a734b2d2
dan wiseman <dan@thewisemans.io>
2022-03-01 10:53:53 -0500

    initial creation from mvn

---
+++ src/test/java/com/github/danwiseman/kafka/connect/scryfall/DocumentationTest.java
@@ -0,0 +1,7 @@
+package com.github.danwiseman.kafka.connect.scryfall;
+
+import com.github.jcustenborder.kafka.connect.utils.BaseDocumentationTest;
+
+public class DocumentationTest extends BaseDocumentationTest {
+
+}
