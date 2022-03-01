14a40b8135184ab0963e17a9e69ecc042d0623e3
dan wiseman <dan@thewisemans.io>
2022-03-01 16:42:13 -0500

    added tests for the connector

--- src/test/java/com/github/danwiseman/kafka/connect/scryfall/ScryfallSourceConnectorTest.java
+++ src/test/java/com/github/danwiseman/kafka/connect/scryfall/ScryfallSourceConnectorTest.java
@@ -1,10 +1,29 @@
 package com.github.danwiseman.kafka.connect.scryfall;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 
+import static com.github.danwiseman.kafka.connect.scryfall.ScryfallSourceConnectorConfig.*;
+import static org.junit.Assert.assertEquals;
+
 public class ScryfallSourceConnectorTest {
+
+  private Map<String, String> initialConfig() {
+        Map<String, String> config = new HashMap<>();
+        config.put(RATE_LIMIT_CONFIG, "100");
+        config.put(TOPIC_CONFIG, "scryfall_cards");
+
+        return (config);
+    }
+
   @Test
-  public void test() {
-    // Congrats on a passing test!
+  public void taskConfigsShouldReturnOneTaskConfig() {
+    ScryfallSourceConnector scryfallSourceConnector = new ScryfallSourceConnector();
+    scryfallSourceConnector.start(initialConfig());
+
+    assertEquals(scryfallSourceConnector.taskConfigs(1).size(), 1);
+    assertEquals(scryfallSourceConnector.taskConfigs(10).size(), 1);
   }
 }
