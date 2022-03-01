2bd3c6f8fd7fbae7ea749e67956e02bbb8b49170
dan wiseman <dan@thewisemans.io>
2022-03-01 17:17:07 -0500

    started to add the schemas

---
+++ src/main/java/com/github/danwiseman/kafka/connect/scryfall/ScryfallSchemas.java
@@ -0,0 +1,116 @@
+package com.github.danwiseman.kafka.connect.scryfall;
+
+import org.apache.kafka.common.security.plain.internals.PlainSaslServer.PlainSaslServerFactory;
+
+public class ScryfallSchemas {
+    
+
+    // Card Fields
+    public static final String CARD_ID_FIELD = "id";
+    public static final String CARD_LANG_FIELD = "lang";
+    public static final String CARD_ARENA_ID_FIELD = "arena_id";
+    public static final String CARD_MTGO_ID = "mtgo_id";
+    public static final String CARD_MTGO_FOIL_ID = "mtgo_foil_id";
+    public static final String CARD_MULTIVERSE_IDS = "multiverse_ids";
+    public static final String CARD_OBJECT = "object";
+    public static final String CARD_ORACLE_ID = "oracle_id";
+    public static final String CARD_PRINTS_SEARCH_URI = "prints_search_uri";
+    public static final String CARD_RULINGS_URI = "rulings_uri";
+    public static final String CARD_SCRYFALL_URI = "scryfall_uri";
+    public static final String CARD_URI = "uri";
+
+    // GAMEPLAY FIELDS
+    public static final String CARD_ALL_PARTS = "all_parts";
+    public static final String CARD_FACES = "card_faces";
+    public static final String CARD_CMC = "cmc";
+    public static final String CARD_COLOR_IDENTITY = "color_identity";
+    public static final String CARD_COLOR_INDICATOR = "color_indicator";
+    public static final String CARD_COLORS = "colors";
+    public static final String CARD_EDHREC_RANK = "edhrec_rank";
+    public static final String CARD_HAND_MODIFIER = "hand_modifier";
+    public static final String CARD_KEYWORDS = "keywords";
+    public static final String CARD_LAYOUT = "layout";
+    public static final String CARD_LEGALITIES = "legalities";
+    public static final String CARD_LIFE_MODIFIER = "life_modifier";
+    public static final String CARD_LOYALTY = "loyalty";
+    public static final String CARD_MANA_COST = "mana_cost";
+    public static final String CARD_NAME = "name";
+    public static final String CARD_ORACLE_TEXT = "oracle_text";
+    public static final String CARD_OVERSIZED = "oversized";
+    public static final String CARD_POWER = "power";
+    public static final String CARD_PRODUCED_MANA = "produced_mana";
+    public static final String CARD_RESERVED = "reserved";
+    public static final String CARD_TOUGHNESS = "toughness";
+    public static final String CARD_TYPE_LINE = "type_line";
+
+    // PRINT FIELDS
+    public static final String CARD_ARTIST = "artist";
+    public static final String CARD_BOOSTER = "booster";
+    public static final String CARD_BORDER_COLOR = "border_color";
+    public static final String CARD_BACK_ID = "card_back_id";
+    public static final String CARD_COLLECTOR_NUMBER = "collector_number";
+    public static final String CARD_CONTENT_WARNING = "content_warning";
+    public static final String CARD_DIGITAL = "digital";
+    public static final String CARD_FINISHES = "finishes";
+    public static final String CARD_FLAVOR_NAME = "flavor_name";
+    public static final String CARD_FLAVOR_TEXT = "flavor_text";
+    public static final String CARD_FRAME_EFFECTS = "frame_effects";
+    public static final String CARD_FRAME = "frame";
+    public static final String CARD_FULL_ART = "full_art";
+    public static final String CARD_GAMES = "games";
+    public static final String CARD_HIGHRES_IMAGE = "highres_image";
+    public static final String CARD_ILLUSTRATION_ID = "illustration_id";
+    public static final String CARD_IMAGE_STATUS = "image_status";
+    public static final String CARD_PRINTED_NAME = "printed_name";
+    public static final String CARD_PRINTED_TEXT = "printed_text";
+    public static final String CARD_PRINTED_TYPE_LINE = "printed_type_line";
+    public static final String CARD_PROMO = "promo";
+    public static final String CARD_PROMO_TYPES = "promo_types";
+    public static final String CARD_RARITY = "rarity";
+    public static final String CARD_RELEASED_AT = "released_at";
+    public static final String CARD_REPRINT = "reprint";
+    public static final String CARD_SCRYFALL_SET_URI = "scryfall_set_uri";
+    public static final String CARD_SET_NAME = "set_name";
+    public static final String CARD_SET_SEARCH_URI = "set_search_uri";
+    public static final String CARD_SET_TYPE = "set_type";
+    public static final String CARD_SET_URI = "set_uri";
+    public static final String CARD_SET = "set";
+    public static final String CARD_SET_ID = "set_id";
+    public static final String CARD_STORY_SPOTLIGHT = "story_spotlight";
+    public static final String CARD_TEXTLESS = "textless";
+    public static final String CARD_VARIATION = "variation";
+    public static final String CARD_VARIATION_OF = "variation_of";
+    public static final String CARD_SECURITY_STAMP = "security_stamp";
+    public static final String CARD_WATERMARK = "watermark";
+    public static final String CARD_PREVIEW_PREVIEWED_AT = "preview.previewed_at";
+    public static final String CARD_PREVIEW_SOURCE_URI = "preview.source_uri";
+    public static final String CARD_PREVIEW_SOURCE = "preview.source";
+
+    // Card Face Objects
+    public static final String CARD_FACE_ARTIST = "artist";
+    public static final String CARD_FACE_CMC = "cmc";
+    public static final String CARD_FACE_COLOR_INDICATOR = "color_indicator";
+    public static final String CARD_FACE_COLORS = "colors";
+    public static final String CARD_FACE_FLAVOR_TEXT = "flavor_text";
+    public static final String CARD_FACE_ILLUSTRATION_ID = "illustration_id";
+    public static final String CARD_FACE_IMAGE_URIS = "image_uris";
+    public static final String CARD_FACE_LAYOUT = "layout";
+    public static final String CARD_FACE_LOYALTY = "loyalty";
+    public static final String CARD_FACE_MANA_COST = "mana_cost";
+    public static final String CARD_FACE_NAME = "name";
+    public static final String CARD_FACE_OBJECT = "object";
+    public static final String CARD_FACE_ORACLE_ID = "oracle_id";
+    public static final String CARD_FACE_ORACLE_TEXT = "oracle_text";
+    public static final String CARD_FACE_POWER = "power";
+    public static final String CARD_FACE_PRINTED_NAME = "printed_name";
+    public static final String CARD_FACE_PRINTED_TEXT = "printed_text";
+    public static final String CARD_FACE_PRINTED_TYPE_LINE = "printed_type_line";
+    public static final String CARD_FACE_TOUGHNESS = "toughness";
+    public static final String CARD_FACE_TYPE_LINE = "type_line";
+    public static final String CARD_FACE_WATERMARK = "watermark";
+
+    // image uris
+    // prices
+    // purchase uris
+    // related uris
+}
